515320b13065c66805726f4e958dca0d011b27e3
Brian <juniortoad1738@gmail.com>
2016-07-28 19:13:05 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var roll(){
+  
+}
+
+
+
+
+var userChoice = prompt("Do you pick Rock, Paper, or Scissors?");
+var computerChoice = Math.random();
+  if (computerChoice < 0.34){
+    computerChoice === "Rock";
+  } else if {(computerChoice > .67)
+    computerChoice === "Paper";
+  } else {
+    computerChoice === "Scissors"  }
+
+var compare = function(choice1, choice2){
+  var start = compare(userChoice, computerChoice)
+  if (choice1 === "Rock"){
+    if (choice2 === "Paper"){
+      return choice2 + "is the winner!";
+  } else if (choice1 === choice2){
+      return "There is a draw!";
+  } else if (choice1 === "Scissors"){
+      return choice1 + "wins!";
+    } 
+  else if (choice1 === "Paper"){
+      if (choice2 === "Scissors"){
+        return choice2 + "is the winner!";
+    } else if (choice2 === choice1){
+        return "There is a draw!";
+    } else if (choice2 === "Rock"){
+        return choice1 + "wins!";
+    }
+  else if (choice1 === "Scissors"){
+    if (choice2 === "Rock"){
+      return choice2 + " is the winner!";
+    } else if (choice2 === choice1){
+      return "There is a draw!";
+    } else if (choice2 === "Paper"){
+      return choice1 + "wins!";
+    }
+  }
+  } 
+  }
+}
