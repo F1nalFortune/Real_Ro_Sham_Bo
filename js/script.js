318a57ce76f1ca50d2d024f840063c129e12cd39
Brian <juniortoad1738@gmail.com>
2016-07-28 20:20:28 -0600

    Fixed loggs

--- js/script.js
+++ js/script.js
@@ -1,48 +1,49 @@
+
+function compare(choice1, choice2) {
+  if (choice1 === "Rock") {
+    if (choice2 === "Paper"){
+      console.log(choice2 + "is the winner!");
+      return choice2 + "is the winner!";
+    } else if (choice1 === choice2){
+      console.log("There is a draw!");
+      return "There is a draw!";
+    } else if (choice2 === "Scissors"){
+      console.log(choice1 + "wins!");
+      return choice1 + "wins!";
+    } 
+  } else if (choice1 === "Paper") {
+    if (choice2 === "Scissors"){
+      return choice2 + "is the winner!";
+    } else if (choice2 === choice1){
+      return "There is a draw!";
+    } else if (choice2 === "Rock"){
+      return choice1 + "wins!";
+    }
+  } else if (choice1 === "Scissors") {
+    if (choice2 === "Rock"){
+      return choice2 + " is the winner!";
+    } else if (choice2 === choice1){
+      return "There is a draw!";
+    } else if (choice2 === "Paper"){
+      return choice1 + "wins!";
+    }
+  }
+  // start;
+}
+  
+
+
 function go() {
   var userChoice = prompt("Do you pick Rock, Paper, or Scissors?");
   var computerChoice = Math.random();
-    if (computerChoice < 0.34){
-      computerChoice = "Rock";
-    } else if (computerChoice > .67){
-      computerChoice = "Paper";
-    } else {
-      computerChoice = "Scissors";
-    } console.log("Computer:" + computerChoice);
-
-  var compare = function(choice1, choice2){
-    var start = compare(userChoice, computerChoice)
-    if (choice1 === "Rock"){
-      if (choice2 === "Paper"){
-        // console.log(choice2 + "is the winner!");
-        return choice2 + "is the winner!";
-    } else if (choice1 === choice2){
-        // console.log("There is a draw!");
-        return "There is a draw!";
-    } else if (choice1 === "Scissors"){
-        // console.log(choice1 + "wins!");
-        return choice1 + "wins!";
-      }
-    else if (choice1 === "Paper"){
-        if (choice2 === "Scissors"){
-          return choice2 + "is the winner!";
-      } else if (choice2 === choice1){
-          return "There is a draw!";
-      } else if (choice2 === "Rock"){
-          return choice1 + "wins!";
-      }
-    else if (choice1 === "Scissors"){
-      if (choice2 === "Rock"){
-        return choice2 + " is the winner!";
-      } else if (choice2 === choice1){
-        return "There is a draw!";
-      } else if (choice2 === "Paper"){
-        return choice1 + "wins!";
-      }
-    }
-    }
-    }
-    start;
-  }
-  }
-  }
+  if (computerChoice < 0.34){
+    computerChoice = "Rock";
+  } else if (computerChoice > .67){
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  } 
+  console.log("Computer:" + computerChoice);
+  console.log(compare(userChoice, computerChoice));
 }
+
